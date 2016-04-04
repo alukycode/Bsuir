3821a039da68de28c8426987410ce3c4e38fc5c3
Anton Lukyanov <zerobotix@gmail.com>
2016-04-05 00:28:37 +0300

    initial

---
+++ Railway/Railway.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Railway.Web.Startup))]
+namespace Railway.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
